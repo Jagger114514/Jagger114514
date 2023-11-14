a182f5070c78afb3d35afe4a4218e9f2bb1683ca
Jagger <150328841+Jagger114514@users.noreply.github.com>
2023-11-14 19:44:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.button("Reset", label="click to show two lines of texts below", type="primary")
+st.button("Reset", help="click to show two lines of texts below", type="primary")
 if st.button('Let us go'):
     st.write('Success!')
