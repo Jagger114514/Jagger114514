68d7b012aa53cb3698964ef32ed960d0f907b724
Jagger <150328841+Jagger114514@users.noreply.github.com>
2023-11-14 20:19:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 title = st.text_input('Type here to show what you want to show', key=1)
+second = st.text_input('Type here to show what you want to show', key=2)
 
 st.button("reset", help="click here to restart", type="primary")
 if st.button("Just do it!", help="click to show two lines of texts below"):
     st.write(title)
+    st.write(second)
