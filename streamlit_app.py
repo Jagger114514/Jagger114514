811be54d0349179a279e3b98a6fb077b4974cb59
Jagger <150328841+Jagger114514@users.noreply.github.com>
2023-11-14 19:41:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
 st.button("Reset", type="primary")
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+if st.button('Let us go'):
+    st.write('Success!')
