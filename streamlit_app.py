b7b04d6b6aa0c93be272b789b78c0afb4824e8c4
Jagger <150328841+Jagger114514@users.noreply.github.com>
2023-11-14 20:11:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-title_one = st.text_input('Type here to show what you want to show')
-title_two = st.text_input('Type here to show what you want to show')
+title = st.text_input('Type here to show what you want to show')
+title = st.text_input('Type here to show what you want to show')
 
 if st.button("Just do it!", help="click to show two lines of texts below"):
-    st.write('The current movie title is', title_one)
+    st.write('The current movie title is', title)
 
 st.button("reset", help="click here to restart", type="primary")
