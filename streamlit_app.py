4ac0c425effb8971159d46c36713a9bb2a64e487
Jagger <150328841+Jagger114514@users.noreply.github.com>
2023-11-14 20:06:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.button("reset", help="click here to restart", type="primary")
 if st.button("Let us go", help="click to show two lines of texts below"):
     st.write('Success!')
+    
+title = st.text_input('Type here to show what you want to show')
+st.write('The current movie title is', title)
